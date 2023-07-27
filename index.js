39fe6896cec1c25baa38427d08c8262aa2ef45be
Jacksen Nillson <nillsonjacksen@gmail.com>
2023-07-27 08:57:19 +0800

    added continually changing line color and width, draw function finished, all code well commented for future readability

--- index.js
+++ index.js
@@ -9,19 +9,29 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 100;
+ctx.lineWidth = 10;
+
+// globalCompositeOperation - determines how shapes and images are drawn onto the canvas. Source is what your drawing / Destination is whats already on the canvas.
+ctx.globalCompositeOperation = 'luminosity'; /* Other interesting values to try: lighter copy xor destination-over multiply darken overlay color-burn */
 
 // isDrawing is to be used to toggle drawing function - mouse movement only draws when mouse button is held down
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 
+// initalize hue and direction variables that will be used to adjust color and line size while drawing
+let hue = 0;
+let direction = true;
+
+
 function draw(e) {
     if(!isDrawing) {
         return; // Prevents the function running when it is not a mouseDown event
     }
+    
+    /* console.log(e); */ //Can be uncommented for debugging purposes
 
-    console.log(e);
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 
     // Start line from
     ctx.beginPath();
@@ -32,12 +42,29 @@ function draw(e) {
 
     [lastX, lastY] = [e.offfsetX, e.offsetY]; //Object destructuring - Updates lastX & lastY to where the line last ended
 
+    //Increments the hue for each move along lastX / lastY - If statement resets the hue every time it reaches 360
+    hue++; 
+    if (hue >= 360) {
+        hue = 0;
+    }
+
+    // Acts as a conditional toggle for the direction variable
+    if (ctx.lineWidth >= 100 || ctx.lineWidth <=1) {
+        direction = !direction;
+    }
+    // If direction = true lineWidth increments / false lineWidth decrements
+    if (direction) {
+        ctx.lineWidth++;
+    } else {
+        ctx.lineWidth--;
+    }
 }
+
 
 // Listens for mousedown event on the canvas and updates the boolean value of the isDrawing variable to true
 canvas.addEventListener('mousedown', (e) => {
     isDrawing = true;
-    // Updates the lastX and LastY to the location where the mousedown event happened
+    // Updates the lastX and LastY to the location where the mousedown event happened - Draw function will start from here
     [lastX, lastY] = [e.offfsetX, e.offsetY];
 });
 
